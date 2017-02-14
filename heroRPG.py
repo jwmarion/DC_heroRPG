549df849ff3b25c71cece4d1b94282a0109d885f
James M <jwmarion@gmail.com>
2017-02-14 15:54:33 -0500

    Made goblin into a zombie. Zombie may need a nerf.

--- heroRPG.py
+++ heroRPG.py
@@ -16,8 +16,14 @@ class character(object):
     def attack(self, character):
         character.health -= self.power
         print "%s does %d damage to the %s." % (self.type, self.power, character.type)
-        if character.health <= 0:
-            print "The %s is dead" % character.type
+        if self.type != "Zombie":
+            if character.health <= 0:
+                print "The %s is dead" % character.type
+        else:
+            print "ZOMBIE IS IMMORTAL"
+            self.health += 100
+            if character.health <= 0:
+                print "The %s is dead" % character.type
 
 class hero(character):
     def __init__(self,health,power):
@@ -31,23 +37,30 @@ class goblin(character):
         self.power = power
         self.type = "Goblin"
 
+class zombie(character):
+    def __init__(self,health,power):
+        self.health = health
+        self.power = power
+        self.type = "Zombie"
+
 def main():
     h = hero(10,5)
     g = goblin(6,2)
+    z = zombie(6,1)
 
 
-    while g.health > 0 and h.health > 0:
+    while z.health > 0 and h.health > 0:
 
 
         print
         print "What do you want to do?"
-        print "1. fight goblin"
+        print "1. fight monster"
         print "2. do nothing"
         print "3. flee"
         print "> ",
         input = raw_input()
         if input == "1":
-            h.attack(g)
+            h.attack(z)
 
         elif input == "2":
             pass
@@ -57,7 +70,7 @@ def main():
         else:
             print "Invalid input %r" % input
 
-        if g.health > 0:
-            g.attack(h)
+        if z.health > 0:
+            z.attack(h)
 
 main()
